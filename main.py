5abe8e803b482effc7b481ae7d2456260a2e9e82
Lucas Nolon Kochenburger <lucasnolon1234@gmail.com>
2024-04-28 15:34:00 -0300

    Add breadthFirstSearch solve method

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+from collections import deque
+
 class SlidingPuzzle:
-    def __init__(self, puzzle):
-        self.puzzle = puzzle  # Inicializa o quebra-cabeça com a configuração inicial
+    def __init__(self):
+        # Solicita ao usuário a configuração inicial do quebra-cabeça
+        initialPositionInput = input("Digite um conjunto de 9 números para posição inicial ou 'enter' para utilziar a default (213804756): ")
+
+        # Verifica se é valida
+        if  len(initialPositionInput) == 9:
+            self.puzzle = [int(char) for char in initialPositionInput]
+        else:
+            self.puzzle = [2, 1, 3, 8, 0, 4, 7, 5, 6]
+
         self.size = 3  # Define o tamanho do quebra-cabeça (3x3)
         self.empty_pos = self.puzzle.index(0)  # Localiza a posição do espaço vazio (0)
-        self.won = False
+        self.solved = False # Define que não foi resolvido
 
     def move(self, direction):
         # A função move() permite mover o espaço vazio em uma das quatro direções: 'w' (cima), 's' (baixo), 'a' (esquerda), 'd' (direita)
@@ -26,45 +36,103 @@ class SlidingPuzzle:
                 self.puzzle[self.empty_pos], self.puzzle[self.empty_pos + 1] = self.puzzle[self.empty_pos + 1], self.puzzle[self.empty_pos]
                 self.empty_pos += 1
 
-    def checkIfWon(self):
+    def checkIfSolved(self, moves):
         if(self.puzzle == [1, 2, 3, 8, 0, 4, 7, 6, 5]):
-            self.won = True
+            self.solved = True
+
+            print("\n* * * * * Solucionado! * * * * *")
+            print("\nMovimentos para a solução:", moves)
+            print("\nEstado final:")
+            self.display()
 
     def display(self):
         # A função display() imprime o estado atual do quebra-cabeça na tela linha por linha
         for i in range(self.size):
             print(self.puzzle[i*self.size:(i+1)*self.size])
 
+    def manual(self):
+        # Entra em um loop onde exibe o quebra-cabeça, solicita um movimento do usuário e executa o movimento
+        # O loop continua até que o usuário digite 'sair' ou soluciona o jogo
+        moves = []
+        while not self.solved:
+            print("\n")
+            self.display()
+            print("\nUse as teclas w a s d para jogar")
+            directionInput = input("Digite uma direção (w, a, s, d) ou 'sair' para sair: ")
+
+            if directionInput == 'sair':
+                break
+            elif directionInput in ['w', 'a', 's', 'd']:
+                moves.append(directionInput)
+                self.move(directionInput)
+                self.checkIfSolved(moves)
+            else:
+                print("\nMovimento inválido. Tente novamente.")
+
+    def breadthFirstSearch(self): # Busca em largura (amplitude) - Sem informação
+        queue = deque([(self.puzzle, [])])  # Inicializa a fila com o estado inicial e uma lista vazia de movimentos
+        visited = set()  # Conjunto para armazenar os estados visitados
+
+        while queue:
+            current_state, moves = queue.popleft()  # Pega o próximo estado e sua lista de movimentos
+            self.puzzle = current_state  # Atualiza o estado do quebra-cabeça para o estado atual da fila
+            self.empty_pos = self.puzzle.index(0)  # Atualiza a posição do espaço vazio
+
+            # Verifica foi resolvido
+            self.checkIfSolved(moves)
+            if self.solved:
+                break
+
+            visited.add(tuple(self.puzzle))  # Adiciona o estado atual aos estados visitados
+
+            # Gera todos os possíveis movimentos a partir do estado atual
+            possible_moves = []
+            if self.empty_pos >= self.size:  # Pode mover para cima
+                possible_moves.append(('w', self.empty_pos - self.size))
+            if self.empty_pos < self.size * (self.size - 1):  # Pode mover para baixo
+                possible_moves.append(('s', self.empty_pos + self.size))
+            if self.empty_pos % self.size != 0:  # Pode mover para a esquerda
+                possible_moves.append(('a', self.empty_pos - 1))
+            if (self.empty_pos + 1) % self.size != 0:  # Pode mover para a direita
+                possible_moves.append(('d', self.empty_pos + 1))
+
+            # Adiciona os novos estados gerados à fila, junto com os movimentos até esse estado
+            for move, new_empty_pos in possible_moves:
+                new_state = self.puzzle[:]  # Cria uma cópia do estado atual
+                new_state[self.empty_pos], new_state[new_empty_pos] = new_state[new_empty_pos], new_state[self.empty_pos]
+                if tuple(new_state) not in visited:  # Verifica se o novo estado não foi visitado antes
+                    queue.append((new_state, moves + [move]))  # Adiciona o novo estado à fila com os movimentos até ele
+
+        if not self.solved:
+            print("\n* * * * * Não foi possível encontrar uma solução * * * * *")
+
+    def mainResolutionMethods(self):
+        # Controla o fluxo do jogo,, entra em um loop onde exibe os métodos para resolver o quebra-cabeça
+        # Ao escolher um método direciona a execução para a função que resolve com o método selecionado
+        # O loop continua até que o usuário escolha a opção de sair ou seja resolvido por um dos metodos
+        while not self.solved:
+            print("\nMétodos de resolução:")
+            print("1 - Manual")
+            print("2 - Busca em largura (amplitude) - Sem informação")
+            print("3 - Busca pela melhor escolha - Sem informação (Ainda não desenvolvido)")
+            print("4 - Sair")
+            resolutionMethod = input("Informe o método de resolução: ")
+
+            if resolutionMethod == '1':
+                self.manual()
+            elif resolutionMethod == '2':
+                self.breadthFirstSearch()
+            elif resolutionMethod == '4':
+                break
+            else:
+                print("\nMétodo inválido, tente novamente")
 
 def main():
-    # A função main() controla o fluxo do jogo, inicialmente solicitando ao usuário a configuração inicial do quebra-cabeça
-    # Em seguida, entra em um loop onde exibe o quebra-cabeça, solicita um movimento do usuário e executa o movimento
-    # O loop continua até que o usuário digite 'sair'
+    # Inicia uma instancia de SlidingPuzzle
+    slidingPuzzle = SlidingPuzzle()
 
-    initialPositionInput = input("Digite um conjunto de 9 números para posição inicial ou 'enter' para utilziar a default (213804756): ")
-
-    if  len(initialPositionInput) == 9:
-        initialPostion = [int(char) for char in initialPositionInput]
-    else:
-        initialPostion = [2, 1, 3, 8, 0, 4, 7, 5, 6]
-
-    slidingPuzzle = SlidingPuzzle(initialPostion)
-
-    while not slidingPuzzle.won:
-        slidingPuzzle.display()
-        print("Use as teclas w a s d para jogar")
-        directionInput = input("Digite uma direção (w, a, s, d) ou 'sair' para sair: ")
-
-        if directionInput == 'sair':
-            break
-        elif directionInput in ['w', 'a', 's', 'd']:
-            slidingPuzzle.move(directionInput)
-            slidingPuzzle.checkIfWon()
-        else:
-            print("Movimento inválido. Tente novamente.")
-
-    if(slidingPuzzle.won):
-        print("* * * * * Parabéns você ganhou! * * * * *")
+    # Chama a função de para selecionar método de resolução
+    slidingPuzzle.mainResolutionMethods()
 
 if __name__ == "__main__":
     main()
